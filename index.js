9de9356c64e380030eabc0914cd1ad77a006aaa4
islammd39e <islammdrahat308@gmail.comm>
2023-01-17 23:55:48 +0600

    query api data

--- index.js
+++ index.js
@@ -7,19 +7,32 @@ app.use(cors())
 app.use(express.json())
 
 const users = [
-  {"id": 1, "name":"saba", "email": 'saba@.com'},
-  {"id": 2, "name":"sabala", "email": 'sabala@.com'},
-  {"id": 3, "name":"sabnoor", "email": 'sabnoor@.com'},
-  {"id": 4, "name":"soon", "email": 'soon@.com'},
-  {"id": 5, "name":"sani", "email": 'sani@.com'},
-  {"id": 6, "name":"saza", "email": 'saza@.com'}
+  {id: 1,password:123, username:"saba"},
+  {id: 2,password:123, username:"sabala"},
+  {id: 3,password:123, username:"sabnoor"},
+  {id: 4,password:123, username:"soon"},
+  {id: 5,password:123, username:"sani"},
+  {id: 6,password:123, username:"saza"}
 ]
 
 app.get('/users', (req, res)=>{
-  res.send(users)
+  if(req.query.username){
+    const search = req.query.username
+       const filter = users.filter(us => us.username.indexOf(search) >= 0)
+       res.send(filter)
+  }else{
+
+    res.send(users)
+  }
 })
 
-
+app.post('/users', (req, res)=>{
+  const user = req.body;
+  user.id = users.length + 1
+  users.push(user)
+  res.send(user)
+  // console.log(user);
+})
 
 app.get('/', (req, res) => {
   res.send('node server is running')
